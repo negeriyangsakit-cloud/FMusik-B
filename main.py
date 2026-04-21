ffa4fd910405bc818919117af337d842e2d02dfc
negeriyangsakit-cloud <email-kamu@gmail.com>
2026-04-21 23:21:14 +0700

    Menambahkan file awal main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+def main():
+    print("==============================")
+    print("   FMusik-B - Pemutar Musik   ")
+    print("==============================")
+    print("Status: Aplikasi Siap!")
+    # Nanti kita tambahkan logika pemutar di sini
+    
+if __name__ == "__main__":
+    main()
